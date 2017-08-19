320d0e20d48fa989e22dafb168c4bd2c342e1c03
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2017-08-19 14:07:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,57 +30,53 @@ app.set("view engine", "ejs");
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
 
 var SKILL_NAME = "";
-var GET_FACT_MESSAGE = "Mark Twain once said.  ";
-var HELP_MESSAGE = "  You can say give me another quote, or, say stop to exit.";
-var HELP_REPROMPT = "You can say, give me another quote, or stop to exit.";
+var GET_FACT_MESSAGE = "Yogi Berra once said.  ";
+var HELP_MESSAGE = "  You can say give a quote, or, say stop at any time to exit.";
+var HELP_REPROMPT = "You can say, give me a quote, or stop to exit.";
 var STOP_MESSAGE = "Goodbye!";
 
 //=========================================================================================================================================
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "Whenever you find yourself on the side of the majority, it is time to pause and reflect.", // He was a good friend to many, but not me.",
-    "If you tell the truth, you don't have to remember anything.",
-    "Don't go around saying the world owes you a living. The world owes you nothing. It was here first.",
-    "I have never let my schooling interfere with my education.",
-    "Get your facts first, and then you can distort them as much as you please.",
-    "Reader, suppose you were an idiot. And suppose you were a member of Congress. But I repeat myself.",
-    "Always do right. This will gratify some people and astonish the rest.",
-    "The man who does not read good books has no advantage over the man who cannot read them.",
-    "Clothes make the man. Naked people have little or no influence on society.",
-    "Truth is stranger than fiction, but it is because Fiction is obliged to stick to possibilities; Truth isn't.",
-    "Kindness is the language which the deaf can hear and the blind can see.",
-    "Travel is fatal to prejudice, bigotry, and narrow-mindedness.",
-    "Courage is resistance to fear, mastery of fear - not absence of fear. ",
-    "The report of my death was an exaggeration.",
-    "It's not the size of the dog in the fight, it's the size of the fight in the dog.",
-    "Be careful about reading health books. You may die of a misprint.",
-    "It is better to keep your mouth closed and let people think you are a fool than to open it and remove all doubt.",
-    "Keep away from people who try to belittle your ambitions. Small people always do that, but the really great make you feel that you, too, can become great.",
-    "Age is an issue of mind over matter. If you don't mind, it doesn't matter.",
-    "There are three kinds of lies: lies, damned lies, and statistics.",
-    "You can't depend on your eyes when your imagination is out of focus.",
-    "The secret of getting ahead is getting started.",
-    "Life would be infinitely happier if we could only be born at the age of eighty and gradually approach eighteen.",
-    "The average American may not know who his grandfather was. But the American was, however, one degree better off than the average Frenchman who, as a rule, was in considerable doubt as to who his father was.",
-    "There has been only one Christian. They caught him and crucified him. Early.",
-    "Total abstinence is so excellent a thing that it cannot be carried to too great an extent. In my passion for it I even carry it so far as to totally abstain from total abstinence itself.",
-    "Concerning the difference between man and the jackass: some observers hold that there isn’t any. But this wrongs the jackass.",
-    "Heaven goes by favor. If it went by merit, you would stay out and your dog would go in.",
-    "I am quite sure now that often, very often, in matters concerning religion and politics a man’s reasoning powers are not above the monkey’s.",
-    "I would rather have my ignorance than another man’s knowledge, because I have so much more of it.",
-    "In the first place God made idiots. This was for practice. Then He made school boards.",
-    "Let us endeavor so to live that when we come to die even the undertaker will be sorry.",
-    "Patriot: the person who can holler the loudest without knowing what he is hollering about.",
-    "It is by the goodness of God that in our country we have those three unspeakably precious things: freedom of speech, freedom of conscience, and the prudence never to practice either.",
-    "To be good is noble; but to show others how to be good is nobler and no trouble."
-   
-
+    "When you come to a fork in the road, take it.", // 
+  "You can observe a lot by just watching.",
+  "It ain’t over till it’s over.",
+  "It’s like déjà vu all over again.",
+  "No one goes there nowadays, it’s too crowded.",
+  "Baseball is 90% mental and the other half is physical.",
+  "Always go to other people’s funerals, otherwise they won’t come to yours.",
+  "A nickel ain’t worth a dime anymore.",
+  "We made too many wrong mistakes.",
+  "Congratulations. I knew the record would stand until it was broken.",
+  "You better cut the pizza in four pieces because I’m not hungry enough to eat six.",
+  "You wouldn’t have won if we’d beaten you.",
+  "I usually take a two-hour nap from one to four.",
+  "Never answer an anonymous letter.",
+  "Slump? I ain’t in no slump. I just ain’t hitting.",
+  "How can you think and hit at the same time?",
+  "The future ain’t what it used to be.",
+  "I tell the kids, somebody’s gotta win, somebody’s gotta lose. Just don’t fight about it. Just try to get better.",
+  "It gets late early out here.",
+  "If the people don’t want to come out to the ballpark, nobody’s going to stop them.",
+  "We have deep depth.",
+  "Pair up in threes.",
+  "Why buy good luggage, you only use it when you travel.",
+  "You’ve got to be very careful if you don’t know where you are going, because you might not get there.",
+  "All pitchers are liars or crybabies.",
+  "Even Napoleon had his Watergate.",
+  "Bill Dickey is learning me his experience.",
+  "He hits from both sides of the plate. He’s amphibious.",
+  "It was impossible to get a conversation going, everybody was talking too much.",
+  "I can see how he won twenty-five games. What I don’t understand is how he lost five.",
+  
+  
+    "
 ];
 
 var randomFact = function () {
@@ -112,7 +108,7 @@ alexaApp.launch(function(request, response) {
 //var factArrA = data;
 // var factIndexA = Math.floor(Math.random() * factArrA.length);
 // var randomFactA = factArrA[factIndexA];
-    response.say(GET_FACT_MESSAGE + randomFact()+HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false);
+    response.say(GET_FACT_MESSAGE + randomFact()).reprompt(HELP_REPROMPT).shouldEndSession(true);
 });
 
 alexaApp.intent("GetNewQuoteIntent", {
@@ -122,7 +118,7 @@ alexaApp.intent("GetNewQuoteIntent", {
     ]
   },
                 function (request, response){
-  response.say(GET_FACT_MESSAGE + randomFact() + HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false)});
+  response.say(GET_FACT_MESSAGE + randomFact()).reprompt(HELP_REPROMPT).shouldEndSession(true)});
 
     
    // 'GetNewFactIntent': function () {
@@ -177,7 +173,7 @@ alexaApp.intent("AMAZON.CancelIntent", {
   //              ]
 //  },
   function(request, response) {
-    response.say("Cancelling. Goodbye!").shouldEndSession(true);
+    response.say("Goodbye!").shouldEndSession(true);
   }
  );
 
